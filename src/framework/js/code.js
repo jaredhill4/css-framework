e15739714c4bdfc63dda9830c4644ae380672c14
Jared Hill <jared.hill@wlion.com>
2017-11-09 15:42:23 -0600

    changes for PT-16

--- src/framework/js/code.js
+++ src/framework/js/code.js
@@ -1,33 +1,29 @@
-/**
- * Code
- */
-
-// Vendors
 import $ from 'jquery';
 import 'google-code-prettify/src/prettify';
 import 'google-code-prettify/src/lang-css';
 
-const wrapperClass = 'code';
+const baseClass = 'code';
 const preClass = 'code__pre';
 const codeClass = 'code__code';
-const unstyledCodeClass = 'code__unstyled-code';
+const linesClass = 'code__lines';
 const lineNumbersClass = 'code__line-numbers';
 const lineHighlightClass = 'code__line-highlight';
 const copyBtnClass = 'code__copy-btn';
+const unstyledCodeClass = 'code__unstyled-code';
 
 class Code {
   constructor(element) {
     this.html = $('html');
     this.code = $(element);
+    this.lines = null;
     this.lineHighlight = null;
 
     this.code
       .addClass(`${preClass} prettyprint linenums`)
-      .wrap(`<div class="${wrapperClass}"></div>`)
-      .after(`<textarea class="${unstyledCodeClass}"></textarea>`)
-      .next(`.${unstyledCodeClass}`)
-      .html(this.code.html())
-      .text();
+      .wrap(`<div class="${baseClass}"></div>`)
+      .after(`<textarea class="${unstyledCodeClass}"></textarea>`);
+
+    this.code.next(`.${unstyledCodeClass}`).html(this.code.html()).text();
 
     window.prettyPrint();
 
@@ -35,27 +31,26 @@ class Code {
       .prepend(`<div class="${lineHighlightClass}"></div>`)
       .prepend(`<ol class="${lineNumbersClass}"></ol>`)
       .wrapInner(`<code class="${codeClass}"></code>`)
-      .prepend(`<span class="${copyBtnClass}"></span>`)
-      .find('ol.linenums')
-      .addClass('code__lines')
-      .find('> li')
-      .each((i, el) => {
-        this.code.find(`.${lineNumbersClass}`).append(`<li>${i + 1}</li>`);
-      });
+      .prepend(`<span class="${copyBtnClass}"></span>`);
 
+    this.code.find('ol.linenums').addClass(linesClass);
+
+    this.lines = this.code.find(`.${linesClass}`);
     this.lineHighlight = this.code.find(`.${lineHighlightClass}`);
+
+    this.lines.children('li').each((i, el) => {
+      this.code.find(`.${lineNumbersClass}`).append(`<li>${i + 1}</li>`);
+    });
 
     this._setListeners();
   }
 
   _setListeners() {
     this.html.on('click', event => this.onBodyClick(event));
-    this.code
-      .find('ol.linenums > li')
+    this.lines.on('scroll', event => this.toggleScrollState(event));
+    this.lines
+      .children('li')
       .on('click', event => this.showLineHighlight(event));
-    this.code
-      .find('ol.linenums')
-      .on('scroll', event => this.toggleScrollState(event));
     this.code
       .find(`.${copyBtnClass}`)
       .on('click', event => this.copyCode(event));
@@ -67,16 +62,8 @@ class Code {
     }
   }
 
-  hideLineHighlight(event) {
-    this.lineHighlight.removeClass(`${lineHighlightClass}--visible`);
-  }
-
   showLineHighlight(event) {
-    // this.hideLineHighlight(event);
-
     const self = $(event.currentTarget);
-
-    console.log(this.lineHighlight);
 
     this.lineHighlight
       .css({
@@ -86,9 +73,22 @@ class Code {
       .addClass(`${lineHighlightClass}--visible`);
   }
 
+  hideLineHighlight(event) {
+    this.lineHighlight.removeClass(`${lineHighlightClass}--visible`);
+  }
+
+  toggleScrollState(event) {
+    const self = $(event.currentTarget);
+
+    if (self.scrollLeft() > 0) {
+      this.code.addClass(`${preClass}--scrolled`);
+    } else {
+      this.code.removeClass(`${preClass}--scrolled`);
+    }
+  }
+
   copyCode(event) {
     const self = $(event.currentTarget);
-    //const code = self.closest(`.${preClass}`);
 
     this.code.next(`.${unstyledCodeClass}`).select();
     document.execCommand('Copy');
@@ -97,17 +97,6 @@ class Code {
     setTimeout(() => {
       self.removeClass(`${copyBtnClass}--copied`);
     }, 1500);
-  }
-
-  toggleScrollState(event) {
-    const self = $(event.currentTarget);
-    //const code = self.closest(`.${preClass}`);
-
-    if (self.scrollLeft() > 0) {
-      this.code.addClass(`${preClass}--scrolled`);
-    } else {
-      this.code.removeClass(`${preClass}--scrolled`);
-    }
   }
 }
 
